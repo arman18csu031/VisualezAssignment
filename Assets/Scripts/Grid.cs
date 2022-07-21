05e922c15a348309e88df1d8a11b87c1c9446de5
arman18csu031 <57385463+arman18csu031@users.noreply.github.com>
2022-07-21 17:42:53 +0530

    Final commit
    may have some bugs but the assignment with the asked functionality is completed

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    [SerializeField] GameObject gridPrefab;
+    [SerializeField] int gridSize;
+    public LoadTextures loadtexture;
+    private GameObject[,] grid = new GameObject[8,8]; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("CreateGrid", 2f);
+        CreateGrid(); 
+    }
+    void CreateGrid()
+    {
+        for (int i = 0; i < gridSize; i++)
+        {
+            for (int j = 0; j < gridSize; j++)
+            {
+                grid[i, j] = Instantiate(gridPrefab, new Vector3(i, j, 0), Quaternion.identity);
+                if (i % 2 != 0 && j % 2 != 0 || i % 2 == 0 && j % 2 == 0)
+                {
+                    grid[i,j].GetComponent<SpriteRenderer>().sprite = loadtexture.sprite[0];
+                }
+                else
+                {
+                    grid[i, j].GetComponent<SpriteRenderer>().sprite = loadtexture.sprite[1];
+                }
+            }
+        }
+    }
+    void ApplyingTexture(GameObject _grid, int spriteIndex)
+    {
+
+        _grid.GetComponent<SpriteRenderer>().sprite = loadtexture.sprite[spriteIndex];
+
+        
+    }
+
+}
---
+++ Assets/Scripts/LoadTextures.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class LoadTextures : MonoBehaviour
+{
+    [SerializeField] private List<string> url = new List<string>();
+    public List<Sprite> sprite = new List<Sprite>();
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        for(int i = 0; i <url.Count; i++)
+        {
+            StartCoroutine(GetTexture(url[i]));
+        }
+       
+    }
+
+    private IEnumerator GetTexture(string url)
+    {
+        using(UnityWebRequest unityWebRequest = UnityWebRequestTexture.GetTexture(url))
+        {
+            yield return unityWebRequest.SendWebRequest();
+            if(unityWebRequest.isNetworkError || unityWebRequest.isHttpError)
+            {
+                Debug.Log("error: " + unityWebRequest.error);
+            }
+            else
+            {
+                DownloadHandlerTexture downloadHandlerTexture = unityWebRequest.downloadHandler as DownloadHandlerTexture;
+                Debug.Log("Received");
+                Texture2D tex2D = downloadHandlerTexture.texture;
+                Sprite tempSprite = Sprite.Create(tex2D, new Rect(0, 0, 1, 1), new Vector2(.5f, .5f), 100);
+                sprite.Add(tempSprite);
+            }
+        }
+    }
+}
